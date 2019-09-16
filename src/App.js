ec88ca29df2497fcdaa9477bc6665d35df4c3cc8
gaorock530 <gaorock530@hotmail.com>
2019-09-17 03:20:46 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        中原青少年艺术赛事网
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );

7c151bbeea66660ea55004ad5373834b31209a42
gaorock530 <gaorock530@hotmail.com>
2019-09-17 23:57:43 +0800

    add server

---
+++ server.js
@@ -0,0 +1,22 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.set('x-powered-by', false);
+app.use(express.static(__dirname + '/build'));
+
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '/ssl/2_www.yingxitech.com.key')),
+  cert: fs.readFileSync(path.join(__dirname, '/ssl/1_www.yingxitech.com_bundle.crt'))
+};
+
+const server = https.createServer(options, app);
+
+
+server.listen(9999, (e) => {
+  console.log(e || 'Server is running on port 9999.');
+});

96a21f99ae99052c356d310d943ff46209b71fa4
gaorock530 <gaorock530@hotmail.com>
2019-09-18 23:43:06 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends React.PureComponent {
       wx.updateAppMessageShareData({ 
         title: '中原青少年艺术赛事网', // 分享标题
         desc: '中原青少年艺术赛事网的描述', // 分享描述
-        link: 'https://yingxitech.com/henanbisai/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: 'https://yingxitech.com/henanbisai/apple-touch-icon.png', // 分享图标
+        link: 'https://yingxitech.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: 'https://yingxitech.com/apple-touch-icon.png', // 分享图标
         success: function () {
           // 设置成功
         }

9959db3b44923467b688d68b13e14c8c18ac300f
gaorock530 <gaorock530@hotmail.com>
2019-09-19 00:11:43 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -20,11 +20,35 @@ class App extends React.PureComponent {
         success: function () {
           // 设置成功
         }
-      })
+      });
+
+      wx.getLocation({
+        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+        success: function (res) {
+          const latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+          const longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+          const speed = res.speed; // 速度，以米/每秒计
+          const accuracy = res.accuracy; // 位置精度
+          const location = JSON.stringify({latitude, longitude, speed, accuracy});
+          localStorage.setItem('location', location)
+        }
+      });
     });
   }
   
-  
+  openLocation () {
+    const location = localStorage.getItem('location');
+    if (!location) return;
+    const obj = JSON.parse(location);
+    wx.openLocation({
+      latitude: obj.latitude, // 纬度，浮点数，范围为90 ~ -90
+      longitude: obj.longitude, // 经度，浮点数，范围为180 ~ -180。
+      name: '', // 位置名
+      address: '', // 地址详情说明
+      scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+      infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+    });
+  }
   
 
   render () {
@@ -32,7 +56,7 @@ class App extends React.PureComponent {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p onClick={this.openLocation}>
           中原青少年艺术赛事网
           </p>
           
